87d34853897757aa84d3ecb8d39cb1bfe8bc79ee
bgo-surf <swv4power@gmail.com>
2026-04-16 20:42:11 -0300

    Segundo Commit

--- app/src/main/java/com/gob/agendaapp/MainActivity.java
+++ app/src/main/java/com/gob/agendaapp/MainActivity.java
@@ -9,17 +9,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import com.google.firebase.firestore.FirebaseFirestore;//importar link com o fire base
+
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
     public EditText codigo;
     public EditText assunto;
-    public EditText date;
+    public EditText data;
     public EditText descricao;
+    public int cod;
+    public String assunt;
+     public Date dat;
+    public String desc;
+    public Boolean resultadoVerificacao;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_cadastrar);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -30,7 +40,36 @@ public class MainActivity extends AppCompatActivity {
     public void coletarDadosFormulario(View view){
         this.codigo   =     findViewById(R.id.editTextText);//coleta os dados da tela pasando para variavel codigo
         this.assunto  =   findViewById(R.id.editTextText2);
-        this.date   =      findViewById(R.id.editTextText3);
+        this.data   =      findViewById(R.id.editTextText3);
         this.descricao = findViewById(R.id.editTextTextMultiLine);
-    }
+
+        this.verificarVazio(codigo, assunto, data, descricao);
+        this.resultadoVerificacao = this.verificarVazio(codigo, assunto, data, descricao); //verificar se os campos estão vazio
+        if(this.resultadoVerificacao == true) {
+        }else{
+            this.cod = Integer.parseInt(this.codigo.getText().toString());
+            this.assunt = (this.assunto.getText().toString());
+
+        }//fim do metodo
+
+    }//fim coletar dados
+
+    //Verifica se estar vazio metodo
+    public boolean verificarVazio(EditText codigo, EditText assunto, EditText data, EditText descricao) {
+        if (codigo.getText().toString().isEmpty() ||
+                assunto.getText().toString().isEmpty() ||
+                data.getText().toString().isEmpty() ||
+                descricao.getText().toString().isEmpty()
+        ) {
+            return true;
+        }//fim do if
+        return  false;
+    }//fim do metodo verificar vazioi
+
+    public void cadastra (View view){
+        this.coletarDadosFormulario(view);//chama o metodo de coletar dados
+
+    }//fim dometodo cadastrar
+
+
 }//fim da classe
